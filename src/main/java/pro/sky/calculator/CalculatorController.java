2b52963644543fe166e3aafa9ca45b76ba764fe8
wlad184 <evel184@yandex.ru>
2023-03-19 09:05:46 +0700

    Calculator ДЗ

--- src/main/java/pro/sky/calculator/CalculatorController.java
+++ src/main/java/pro/sky/calculator/CalculatorController.java
@@ -1,7 +1,9 @@
 package pro.sky.calculator;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/calculator")
 public class CalculatorController {
 }
